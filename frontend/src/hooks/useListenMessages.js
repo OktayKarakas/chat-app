cf4e02ad0949670e51afeda5e2377b68899ba375
Oktay Karakaş <109868198+OktayKarakas@users.noreply.github.com>
2024-08-03 13:34:00 +0300

    fix

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -1,23 +1,32 @@
 import { useEffect } from "react";
-
 import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useConversation";
 
 import notificationSound from "../assets/sounds/notification.mp3";
 
 const useListenMessages = () => {
-	const { socket } = useSocketContext();
-	const { messages, setMessages } = useConversation();
+  const { socket } = useSocketContext();
+  const { selectedConversation, setMessages } = useConversation();
 
-	useEffect(() => {
-		socket?.on("newMessage", (newMessage) => {
-			newMessage.shouldShake = true;
-			const sound = new Audio(notificationSound);
-			sound.play();
-			setMessages([...messages, newMessage]);
-		});
+  useEffect(() => {
+    const handleMessage = (newMessage) => {
+      if (newMessage.conversationId === selectedConversation?._id) {
+        newMessage.shouldShake = true;
+        setMessages((prevMessages) => [...prevMessages, newMessage]);
+      }
+    };
 
-		return () => socket?.off("newMessage");
-	}, [socket, setMessages, messages]);
+	const sound = new Audio(notificationSound);
+	sound.play();
+
+    if (socket) {
+      socket.on("newMessage", handleMessage);
+
+      return () => {
+        socket.off("newMessage", handleMessage);
+      };
+    }
+  }, [socket, selectedConversation?._id, setMessages]);
 };
+
 export default useListenMessages;
