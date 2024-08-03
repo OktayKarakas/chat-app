5acc6178b26e3fe03a528eb6147884060b5716bb
Oktay Karakaş <109868198+OktayKarakas@users.noreply.github.com>
2024-08-03 11:20:13 +0300

    bug

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://chat-app-5i6l.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
